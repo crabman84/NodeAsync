691cec4b5103eb7079d977383c149dcae79e7127
Luke Gavin | SKOPOS CONNECT <luke.gavin@skopos-connect.de>
2019-03-17 22:39:19 +0100

    andded handlebars for dynamic templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,41 +1,51 @@
 'use strict';
 
+//Core node applications
 const path = require('path');
 
+//modules downloaded from npm
 const express = require('express');
 
-const publicDir = path.join(__dirname, '../public')
-
-console.log(__dirname);
-console.log(publicDir);
-
 const app = express();
+
+//express config
+
+//define paths for express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 // const htmlHelp = path.join(__dirname, '../public/help.html')
 
+// create dynamic templates with handlebars template engine for views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
 //customize the server
-//treat this like a black box for now
-app.use(express.static(publicDir))
+//Setup static directory to serve
+app.use(express.static(publicDir));
 
-// app.get ('', (req, res) => {
-//     res.send()
-// });
+app.get('', (req, res) => {
+      res.render('index', {
+          title: 'Weather App',
+          name: 'CraboRoboto'
+      })
+})
 
 
-// app.use(express.static(publicDir));
+//TODO: Routing :::::::::DDDDDDD
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        name: 'CraboRoboto'
+    })
+})
 
-
-// app.get ('/help', (req, res) => {
-//     res.send(
-//         {
-//             name: 'crabMan',
-//             age: 2
-//         })
-// });
-
-// app.get ('/about', (req, res) => {
-//     res.send('<h1>About Page</h1>')
-// });
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'CraboRoboto',
+        msg: 'Go back to home, we are not finished here.'
+    })
+})
 
 app.get ('/weather', (req, res) => {
     res.send({
@@ -44,10 +54,9 @@ app.get ('/weather', (req, res) => {
     })
 });
 
-// app.com
-// app.com/help
-// app.com/about
 
+
+//TODO: Upload web code to port 3000
 app.listen(3000, ()=>{
     console.log('server is up on port 3000');
 });
